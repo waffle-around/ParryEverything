d76de1ea10ca5fc9c1847aa43eda4f60faf0d94b
waffle-around <wafflearound22@gmail.com>
2026-04-08 16:11:58 -0400

    patch

--- Assets/Scripts/ParryBehavior.cs
+++ Assets/Scripts/ParryBehavior.cs
@@ -6,28 +6,39 @@ public class ParryBehavior : MonoBehaviour
 {
     public GameObject parryBox;
     public Transform spawner;
-    private new AudioSource audio;
+    private AudioSource audioSource;
+
+    public float parryCooldownTime = 2.7f;
+    private float timerElapsed;
 
 
     void Start()
     {
-       audio = GetComponent<AudioSource>();
+       timerElapsed = parryCooldownTime;
+       audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject obj = Instantiate(parryBox, spawner.position, spawner.rotation);
-        }
+        timerElapsed += Time.deltaTime;
 
+        if (timerElapsed >= parryCooldownTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject obj = Instantiate(parryBox, spawner.position, spawner.rotation);
+                timerElapsed = 0f;
+            }
+
+          
+        }       
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Projectile"))
         {
-            audio.Play();
+            audioSource.Play();
         }
            
     }
